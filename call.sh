06552fe9bbeecf5a321f54768a9e747f457de8fb
Marcos Stefani Rosa <OUR_NAME@example.com>
2021-02-06 20:52:14 -0300

    select info from database

--- call.sh
+++ call.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-response=$(curl -X PUT "http://127.0.0.1:8000/file/" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "text=@file.txt;type=text/plain")
-echo $response
-
 line=$(curl -X PUT "http://127.0.0.1:8000/file/" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "text=@file.txt;type=text/plain" | jq -r '.line')
 highest_occurrence=$(curl -X PUT "http://127.0.0.1:8000/file/" -H "Accept: application/json" -H "Content-Type: multipart/form-data" -F "text=@file.txt;type=text/plain" | jq -r '.highest_occurrence')
-
-echo $highest_occurrence
 
 CREATE="
 CREATE TABLE IF NOT EXISTS reading (
@@ -20,3 +15,6 @@ INSERT INTO reading(line, highest_occurrence)
 VALUES ('${line}', '${highest_occurrence}');"
 
 docker exec -it db-read-file sh -c "psql -U postgres -d postgres -c \"$CREATE\" ";
+
+SELECT="SELECT * FROM reading;"
+docker exec -it db-read-file sh -c "psql -U postgres -d postgres -c \"$SELECT\" ";
